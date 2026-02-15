12d8e3ee4f199b776adf6784d914ebfd110d3a8d
Zilba <basile.lebaudy@gmail.com>
2026-02-16 00:18:03 +0100

    fix lol api

--- src/repository/LolApi.ts
+++ src/repository/LolApi.ts
@@ -2,7 +2,7 @@ import { Champion } from "../models/lol/Champion";
 
 export class LolApi {
     async getAllChampions(): Promise<Champion[]> {
-        const res = await fetch("/src/.generated/champions-en-us.json");
+        const res = await fetch("/.generated/champions-en-us.json");
         const data = await res.json();
         return data.map((champData: any) => new Champion(champData));
     }
